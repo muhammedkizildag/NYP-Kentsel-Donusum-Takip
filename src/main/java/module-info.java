e82b9f15d3a0accbc30835aa1dde00ce4368cbb8
Muhammed Kızıldağ <mhmmd0536@gmail.com>
2025-12-23 23:01:09 +0300

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.nypkentseldonusumtakip {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.nypkentseldonusumtakip to javafx.fxml;
+    exports com.example.nypkentseldonusumtakip;
+}
